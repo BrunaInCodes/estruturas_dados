55f70c7f93206fd66ee70a54f0f4ab7341195d8c
bruna graciele <brunagraciele01@icloud.com>
2024-09-30 10:10:07 -0300

    Listavel.kt

---
+++ Listavel.kt
@@ -0,0 +1,21 @@
+interface Listavel{
+    // Metodos principais
+        fun inserir(posicao: Int, dado: Any?)              // C     // O(N)
+        fun anexar(dado: Any?)                             // C     // O(1)
+        fun  selecionar(posicao: Int): Any?                // R     // O(1)
+        fun  selecionarTodos(): Array<Any?>                // R     // O(N)
+        fun atualizar(posicao: Int, dado: Any?)            // U     // O(1)
+        fun apagar(posicao: Int): Any?                     // D     // O(N)
+        fun limpar()                                       // D     // O(1)
+    
+    // Metodos auxiliares
+        fun imprimir(): String
+        fun estaCheia(): Boolean
+        fun estaVazia(): Boolean
+    }
+    
+    // metodos extas
+    // fun inserirInicio(dado: Any)
+    // fun selecionarInicio(): Any?
+    // fun atualizarFim(dado: Any?)
+    
